48c8af0ccf122793ef4c5746fb2abc24a6ea2cc5
Sunny Shah <sunnykrshah201@gmail.com>
2022-08-08 19:18:24 +0530

    added install button

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var notes = [];
 // Registering all the event handlers when the page loads
 document.addEventListener("DOMContentLoaded", event => {
     if (localStorage.getItem("notes")) {
-        notes = JSON.parse(localStorage.getItem("notes"))
+        notes = JSON.parse(localStorage.getItem("notes"));
     }
     renderNotes();
  
@@ -20,8 +20,36 @@ document.addEventListener("DOMContentLoaded", event => {
         }
     });
 
+
     document.querySelector("#btnLearn").addEventListener("click", event => {
-        location.href = "https://frontendmasters.com";
+        location.href = "https://www.java5cript.com/";
+    })
+
+    let bipEvent = null;
+    window.addEventListener("beforeinstallprompt", event => {
+        event.preventDefault();
+        bipEvent = event;
+    })
+
+    document.querySelector("#btnInstall").addEventListener("click", event => {
+        if (bipEvent) {
+            bipEvent.prompt();
+        } else {
+            // incompatible browser, your PWA is not passing the criteria, the user has already installed the PWA
+            //TODO: show the user information on how to install the app
+            alert("To install the app look for Add to Homescreen or Install in your browser's menu");
+        }
+    })
+
+    document.querySelector("#btnShare").addEventListener("click", event => {
+        let notesString = "";
+        for (let note of notes) {
+            notesString += note + " | "
+        }
+        navigator.share({
+            title: "Codepad",
+            text: notesString
+        })
     })
 })
 
@@ -48,6 +76,7 @@ function renderNotes() {
     })
 }
 
+
 function save() {
-    localStorage.setItem("notes", JSON.stringify(notes))
+    localStorage.setItem("notes", JSON.stringify(notes));
 }
